e1e7628594f60dfe4a5c5faf2f24c92a21469c14
Mathilde Chollon <mathilde.chollon@etu.u-bordeaux.fr>
2024-10-17 10:58:35 +0200

    action 1.5

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -1,19 +1,41 @@
 #ifdef CHANGED
 #include "system.h"
-typedef struct schmurtz {
+typedef struct schmurtz_t {
     int f;
     int arg;
 }Schmurtz;
+
+
+static void StartUserThread(void *schmurtz){
+    for (int i = 0; i < NumTotalRegs; i++)
+        machine->WriteRegister (i, 0);
+    Schmurtz *s = (Schmurtz*) schmurtz;
+    machine->WriteRegister(4,s->arg);
+    DEBUG('s', "Initializing arg in reg 4\n");
+    machine->WriteRegister(PCReg,s->f);
+    DEBUG('s', "Initializing f in PCReg\n");
+    machine->WriteRegister (NextPCReg, machine->ReadRegister(PCReg) + 4);
+    DEBUG('s', "Initializing NextPCReg\n");
+
+    machine->WriteRegister (StackReg,currentThread->space->AllocateUserStack());
+    DEBUG ('s', "Initializing stack register to 0x%x\n",
+           (currentThread->space->NumPages()) * PageSize - 256);
+    free(schmurtz);
+
+    machine->Run();
+
+}
 
 int do_ThreadCreate(int f, int arg){
     Thread *newThread = new Thread("newThread");
     AddrSpace *oldSpace = currentThread->space;
     newThread->space = oldSpace;
-    Schmurtz schmurtz= (Schmurtz) malloc(sizeof(int)*2);
-    schmurtz.f = f;
-    schmurtz.arg = arg;
-    //TODO:init the stack
-    //newThread->Start(StartUserThread, schmurtz);
+    Schmurtz *schmurtz= (Schmurtz*) malloc(sizeof(Schmurtz));
+    schmurtz->f = f;
+    schmurtz->arg = arg;
+  
+    newThread->Start(StartUserThread, schmurtz);
     return -1;
 }
+
 #endif
--- code/userprog/userthread.h
+++ code/userprog/userthread.h
@@ -1,3 +1,4 @@
 #ifdef CHANGED
 extern int do_ThreadCreate(int f, int arg);
+static void StartUserThread(void *schmurtz);
 #endif
