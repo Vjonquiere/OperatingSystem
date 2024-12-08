535b4f583e89f13fe27be1989f49c664085d145d
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2024-12-08 18:00:33 +0100

    test documentation

--- code/test/alloctypescomparison.c
+++ code/test/alloctypescomparison.c
@@ -6,3 +6,15 @@ int main(){
     return 1;
 }
 #endif
+
+/*
+To lauch the test, go in the code folder and execute the makefile with the command make.
+then execute the following command:
+./userprog/nachos -rs 1234 -x test/alloctypescomparison
+
+This test is only here to compare differente page allocation types. You can set two allocation types:
+
+    1) Random allocation: Just launch the test and watch newProcess.svg to see the result
+    2) Classic allocation: un-comment l.142 in addrspace.cc (comment l.143) and watch newProcess.svg to see the result
+
+*/
--- code/test/multipleprocesses.c
+++ code/test/multipleprocesses.c
@@ -9,3 +9,19 @@ int main(){
     return 1;
 }
 #endif
+
+/*
+To lauch the test, go in the code folder and execute the makefile with the command make.
+then execute the following command:
+./userprog/nachos -rs 1234 -x test/multipleprocesses
+
+This test is here to see if we can launch multiple processes that execute a lot of thread each.
+⚠️ Do not forget to increase the number of physical pages in machine.h and the stack size 
+of your user in addrspace.cc.
+Each process is going to launch many threads that will do a simple GetChar before going down.
+The program will exit automaticaly after all processes cleanup.
+
+
+To see all threads, mutexes, processes debugs labels add `-d s` to the launch command 
+
+*/
